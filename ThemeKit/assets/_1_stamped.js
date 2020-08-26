5df6ee475e5a03878a11a97b942cf11272141f35
Bailey Latimer <latimerdesignco@gmail.com>
2020-08-26 18:45:38 -0400

    Bug Fix / Register + Login pages

---
+++ ThemeKit/assets/_1_stamped.js
@@ -0,0 +1,13 @@
+// ============================================================================
+// Third Party / Stamped.io
+// ============================================================================
+$(document).ready(function() {
+    $('.spr-summary-actions-newreview').addClass('button flex');
+    });
+    
+    $(function() {
+    $(".stamped-summary-actions-newreview, .stamped-summary-actions-newquestion, .stamped-pagination a").addClass("btn btn--primary flex");
+    $(".stamped-summary-actions-newquestion").addClass("btn--subtle");
+    $('.stamped-review-avatar[data-author="GoSun"]').closest('.stamped-review-reply').addClass('reply--offical');
+    console.log('Official reply');
+    });
