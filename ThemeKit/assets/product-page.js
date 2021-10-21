03cff53b836069e795d75d22907cbeb7f8a7d79e
Bailey Latimer <latimerdesignco@gmail.com>
2021-10-21 12:13:45 -0700

    Minicart / Show product image as thumbnail

--- ThemeKit/assets/product-page.js
+++
@@ -1,8 +0,0 @@
-
-$(function() {
-  $('.purchase-details').html($('.purchase-details').html().replace("undefined", "")); // Hide text near buy button
-});
-
-$(function() {
-  $('div.option_title').html($('div.option_title').html().replace("Title", "Condition")); // Hide text near buy button
-});
